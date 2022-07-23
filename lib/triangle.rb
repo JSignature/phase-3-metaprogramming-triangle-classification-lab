215d6912a3babaa9b6f2a00fb459d5856b558531
Jimmy Andreas <jimmysignature@gmail.com>
2022-07-22 21:37:34 -0400

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
+require "pry"
+
 class Triangle
   # write code here
+
+  
+def initialize(a, b, c) 
+@a = a
+@b = b 
+@c = c
+end
+
+def kind
+
+  
+  if @a <= 0 or @b <= 0 or @c <= 0
+    raise TriangleError
+    
+    # elsif @a < 0 or @b < 0 or @c < 0
+    
+    #   raise TriangleError
+    
+  elsif @a + @b <= @c or @c + @a <= @b or @b + @c <= @a 
+    
+    raise TriangleError
+    
+  elsif @a == @b and @b == @c
+      :equilateral
+    elsif @a == @b or @b == @c or @c == @a
+      :isosceles
+    elsif @a != @b and @b != @c 
+      :scalene   
+end
+
+
+end
+
+
+
+class TriangleError < StandardError
+  
+end
+
+
 end
